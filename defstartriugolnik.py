f9a904850c0634347eaa7baddee4dd2c9cf7631f
Ros-bund <rostikbondar5@gmail.com>
2026-03-19 18:06:15 +0300

    функция на вызов треугольника из *

---
+++ defstartriugolnik.py
@@ -0,0 +1,16 @@
+# объявление функции
+def draw_triangle(fill, base):
+    mid = base // 2 + 1
+    for i in range(1, mid + 1):
+        print(fill * i)
+        
+    for i in range(mid - 1, 0, -1):
+        print(fill * i)
+    
+
+# считываем данные
+fill = input()
+base = int(input())
+
+# вызываем функцию
+draw_triangle(fill, base)
